46f044e600a920e5a7628e20a6ddcac3f7c0b226
Dasha <dmalahova2@gmail.com>
2025-04-07 10:39:55 +0300

    git initial code

--- Malakhova_project_LR4.cpp
+++ Malakhova_project_LR4.cpp
@@ -3,17 +3,17 @@
 using namespace std;
 
 // Функция для ввода натурального числа
-void inputNaturalNumber(unsigned long long &number) {
+void inputNaturalNumber(unsigned long long& number) {
     cout << "Введите натуральное число X: ";
     cin >> number;
 }
 
 // Функция для ввода цифры
-void inputDigit(int &digit) {
+void inputDigit(int& digit) {
 }
 
 // Функция для проверки, есть ли цифра A в числе X
-void checkDigitPresence(const string& strX, char A, bool &noA, int &countA) {
+bool checkDigitPresence(const string& strX, char A) {
 }
 
 // Функция для подсчета вхождений цифры A в числе X
@@ -24,6 +24,7 @@ int main() {
     unsigned long long x;
     int a;
     int choice;
+
     do {
         cout << "Меню:" << endl;
         cout << "1. Ввести число и цифру для проверки" << endl;
@@ -32,30 +33,31 @@ int main() {
         cin >> choice;
 
         switch (choice) {
-            case 1:
-                inputNaturalNumber(x);
-                inputDigit(a);
-                
-                // Преобразуем число X в строку для проверки
-                string strX = to_string(x);
-                char charA = '0' + a; // Преобразуем цифру в символ
+        case 1:
+            inputNaturalNumber(x);
+            inputDigit(a);
 
-                bool noA;
-                checkDigitPresence(strX, charA, noA);
-                
-                int countA = countDigitOccurrences(strX, charA);
-                cout << "В числе X нет цифры A: " << (noA ? "Да" : "Нет") << endl;
-                cout << "В числе X цифра A встречается " << countA << " раз(а)." << endl;
-                break;
+            // Преобразуем число X в строку для проверки
+            string strX = to_string(x);
+            char charA = '0' + a; // Преобразуем цифру в символ
 
-            case 2:
-                cout << "Выход из программы." << endl;
-                break;
+            bool noA = checkDigitPresence(strX, charA);
+            int countA = countDigitOccurrences(strX, charA);
+            
+            cout << "В числе X нет цифры A: " << (noA ? "Да" : "Нет") << endl;
+            cout << "В числе X цифра A встречается " << countA << " раз(а)." << endl;
+            break;
 
-            default:
-                cout << "Неверный выбор. Пожалуйста, попробуйте снова." << endl;
+        case 2:
+            cout << "Выход из программы." << endl;
+            break;
+
+        default:
+            cout << "Неверный выбор. Пожалуйста, попробуйте снова." << endl;
         }
     } while (choice != 2);
 
     return 0;
 }
+
+
